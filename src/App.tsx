7de631234524c519b3a262562c3c3efbee108af5
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-04-07 22:02:07 +0200

    add call navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import React from "react";
 import Home from "./../src/pages/Homepage";  
+import Navbar from "./components/Navbar";
 
 const App: React.FC = () => {
   return (
     <div>
+      <Navbar /> {/* call lel navbar */}
       <Home />  {/* call lel home */}
     </div>
 
