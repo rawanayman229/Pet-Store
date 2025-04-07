6bac3e2686ac051c94e55bbc74c2ae8b183cf160
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-04-07 22:03:51 +0200

    add call home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import HomeHero from './components/HomeHero';
+import Home from "./../src/pages/Homepage";
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <HomeHero />
+      <Home />
     </>
   );
 };
