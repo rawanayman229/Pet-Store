95749cb32ada868f65cc602b13798c188b7c742f
rawan ayman <rawanrooney229@gmail.com>
2025-04-08 18:07:42 +0200

    edit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import HomeHero from './components/HomeHero';
+import Home from './pages/Homepage';
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <HomeHero />
+      <Home/>
     </>
   );
 };
