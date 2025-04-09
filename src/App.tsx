93acc57b8be7876a55b3ec32dcb52e2df21dbf7b
rawan ayman <rawanrooney229@gmail.com>
2025-04-09 19:32:41 +0200

    Pet Sellers And Banner Sections Added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import Navbar from './components/Navbar';
 import HomeHero from './components/HomeHero';
 import Home from './pages/Homepage';
 import HomeBanner1 from './components/HomeBanner1';
+import ProductCardSection from './components/cardproducts/ProductsSection';
+import PetCardSection from './components/card/CardSection';
+import PetKnowledge from './components/cardKnolge/knowlegeSection';
+import PetsSellers from './components/PetsSellers';
+import HomeBanner2 from './components/HomeBanner2';
+
 
 
 const App: React.FC = () => {
@@ -12,6 +18,12 @@ const App: React.FC = () => {
       <HomeHero />
       <Home/>
       <HomeBanner1 />
+      <ProductCardSection  />
+      <PetsSellers />
+      <HomeBanner2 />
+      <PetKnowledge />
+      
+
     </>
   );
 };
