ddd4bb5c052f6392b9c1a802e634aeca04a28555
rawan ayman <rawanrooney229@gmail.com>
2025-04-11 22:48:54 +0200

    fix banner photos problem

--- src/components/SecondBanner.tsx
+++ src/components/SecondBanner.tsx
@@ -3,9 +3,8 @@ import React from "react";
 const SecondBanner: React.FC = () => {
   return (
     <section
-      className="mt-15 w-full h-auto rounded-xl flex flex-col md:flex-row justify-between items-center px-4 md:px-20 text-center md:text-left"
+      className="mt-15 w-full h-auto rounded-xl bg-[url('/images/banner2Mob.png')]  sm:bg-[url('/images/banner2.png')] flex flex-col md:flex-row justify-between items-center px-4 md:px-20 text-center md:text-left"
       style={{
-        backgroundImage: "url('images/banner2.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
