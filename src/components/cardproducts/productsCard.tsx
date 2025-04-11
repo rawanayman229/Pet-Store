dea40994f37530802d8c48ed7a0fd9a5306f080f
rawan ayman <rawanrooney229@gmail.com>
2025-04-11 21:34:31 +0200

    add files

--- src/components/cardproducts/productsCard.tsx
+++ src/components/cardproducts/productsCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import { Product } from "../../data/products";
 
 interface ProductCardProps {
@@ -6,17 +8,30 @@ interface ProductCardProps {
 }
 
 const ProductCardSection: React.FC<ProductCardProps> = ({ product }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`/product/${product.id}`);
+  };
+
   return (
-    <div className="bg-white rounded-2xl shadow-md p-4 hover:shadow-lg transition duration-300">
-      <div className="flex justify-center mb-4">
-      <img src={product.image} alt={product.name} className="w-full h-56 object-cover" />
+    <div
+      className="bg-white rounded-2xl shadow-md p-4 hover:shadow-lg transition duration-300"
+    >
+      <div className="flex justify-center mb-4 cursor-pointer" onClick={handleNavigate}>
+        <img src={product.image} alt={product.name} className="w-full h-56 object-cover" />
       </div>
 
-      <h3 className="text-base font-bold text-[#00171F] leading-snug">
+      <h3
+        className="text-base font-bold text-[#00171F] leading-snug cursor-pointer hover:underline"
+        onClick={handleNavigate}
+      >
         {product.name}
       </h3>
 
-      <p className="text-sm text-gray-600">Product: {product.category}  . Size: {product.size}</p>
+      <p className="text-sm text-gray-600">
+        Product: {product.category} . Size: {product.size}
+      </p>
 
       <p className="text-[#002A48] font-bold text-lg mt-2">{product.price}</p>
 
@@ -25,6 +40,7 @@ const ProductCardSection: React.FC<ProductCardProps> = ({ product }) => {
       </div>
     </div>
   );
+
 };
 
 export default ProductCardSection;
